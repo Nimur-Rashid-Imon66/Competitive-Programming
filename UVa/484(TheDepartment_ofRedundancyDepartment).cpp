27395744e2c579deb97b8698e93594becf7f5322
Nimur-Rashid-Imon66 <59577412+Nimur-Rashid-Imon66@users.noreply.github.com>
2021-09-04 01:58:13 +0600

    MYCODE

---
+++ UVa/484(TheDepartment_ofRedundancyDepartment).cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define pi             (3.141592653589)
+#define mod            1000000007
+#define nl             endl
+#define pb             push_back
+#define ff             first
+#define ss             second
+#define testcase       {ll t;cin>>t;while(t--){slove();}}
+#define all(c)         c.begin(), c.end()
+#define min3(a, b, c)  min(c, min(a, b))
+#define min4(a, b,c,d) min(d, min(c, min(a, b)))
+#define rrep(i, n)     for(int i=n-1;i>=0;i--)
+#define rep(i,n)       for(int i=0;i<n;i++)
+#define fastIO ios::sync_with_stdio(0); cin.tie(0); 
+typedef long long ll;
+typedef long long ll;
+typedef unsigned long long ull;
+using namespace std;
+
+
+void slove()
+{
+    int n, m;
+    vector<int> arr;
+    map<int, int> mp;
+    while(cin>>n)
+    {
+        if(mp[n]==0)
+        {
+            mp[n]++;
+            arr.pb(n);
+        }
+        else
+            mp[n]++;
+    }
+    for(auto it : arr)
+        cout << it << " " << mp[it] << nl;
+}
+int main(){
+fastIO
+ //testcase
+slove();
+return 0;
+}
